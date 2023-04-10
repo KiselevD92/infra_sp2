117d7ec17dde3589dc038639eeda9136791e2908
KiselevD92 <noproblem2000@yandex.ru>
2023-04-10 16:34:16 +0300

    Iteration #1.1

--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.db.models import Avg
 from django_filters.rest_framework import DjangoFilterBackend
 from django.core.mail import send_mail
-from django.shortcuts import get_object_or_404
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
